9f6a9ebdc683eb210068ac1574c0536320163071
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2025-06-30 18:36:08 +0530

    Implement meditate function and corresponding test cases; add smelt_ore function with tests

--- python/8-Loops/main.py
+++ python/8-Loops/main.py
@@ -1,22 +1,6 @@
-# integer -> integer
-# calculates xp based on current level and previous level xp
-# total xp = current xp + (5 * level)
+def meditate(mana, max_mana, num_potions):
+    while(mana < max_mana and num_potions != 0):
+        num_potions -= 1
+        mana += 1
 
-'''
-examples:
-level current-xp   required
-1     0            5 
-2     5(5+0)       10 = (5 * level)
-3     15(15+10)    15 = (5 * 3)
-4     30(15+10+5)  20 = (5 * 4)
-
-next = prevous xp + (5*level)
-'''
-def calculate_experience_points(level):
-  xp = 0
-  
-  for i in range(1 , level, 1):
-    xp += i * 5
-
-  return xp 
-
+    return mana, num_potions
--- python/9-Lists/main_test.py
+++ python/9-Lists/main_test.py
@@ -1,29 +1,29 @@
 from main import *
 
 run_cases = [
-    (0, 10, 9, [9, 0]),
-    (0, 12, 20, [12, 8]),
-    (1, 100, 80, [81, 0]),
+    (
+        ["Potion", "Iron Bar", "Iron Sword", "Leather Armor"],
+        ["Potion", "Iron Bar", "Iron Sword", "Leather Armor"],
+    ),
+    ([None, None, None, None], [None, None, None, None]),
+    (["Potion", "Iron Ore", None, None], ["Potion", "Iron Bar", None, None]),
 ]
 
 submit_cases = run_cases + [
-    (0, 0, 0, [0, 0]),
-    (1000, 1000, 5, [1000, 5]),
-    (0, 10, 5, [5, 0]),
-    (5, 2000, 500, [505, 0]),
+    (
+        [None, "Iron Ore", None, "Leather Armor"],
+        [None, "Iron Bar", None, "Leather Armor"],
+    ),
 ]
 
 
-def test(input1, input2, input3, expected):
+def test(input1, expected_output):
     print("---------------------------------")
-    print("Inputs:")
-    print(f" *           mana: {input1}")
-    print(f" *       max_mana: {input2}")
-    print(f" *    num_potions: {input3}")
-    print(f"Expecting: mana {expected[0]}, potions {expected[1]}")
-    result_mana, result_potions = meditate(input1, input2, input3)
-    print(f"   Actual: mana {result_mana}, potions {result_potions}")
-    if result_mana == expected[0] and result_potions == expected[1]:
+    print(f"Inputs: {input1}")
+    print(f"Expecting: {expected_output}")
+    result = smelt_ore(input1)
+    print(f"Actual: {result}")
+    if result == expected_output:
         print("Pass")
         return True
     print("Fail")
