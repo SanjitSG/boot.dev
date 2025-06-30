f0cbd1f36f185db1c8e9e7b7987b9e26ddc45537
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2025-07-01 00:06:50 +0530

    concat

--- python/9-Lists/main.py
+++ python/9-Lists/main.py
@@ -1,9 +1,2 @@
-def get_champion_slices(champions):
-    lenght = len(champions)
-    list_one = champions[2:]
-    list_two = champions[:lenght - 1]
-    list_three = champions[::2]
-
-    return list_one, list_two, list_three
-
-    
+def concatenate_favorites(favorite_weapons, favorite_armor, favorite_items):
+    return favorite_weapons + favorite_armor + favorite_items
