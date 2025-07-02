86b56468e54a09c4a6c59b83754e698a23ce9d06
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2025-07-02 23:49:32 +0530

    censor message - dang

--- python/9-Lists/main.py
+++ python/9-Lists/main.py
@@ -1,11 +1,28 @@
-'''def reverse_list(items):
-    reversed_list = []
-    for i in range(len(items)):
-        reversed_list.append(items[len(items) - 1 - i])
-    
-    return reversed_list
+'''
+We need to filter the profanity out of our game's live chat feature! Complete the filter_messages function. It takes a list of chat messages as input and returns 2 new lists:
+
+1. A list of the same messages but with all instances of the word dang removed.
+2. A list containing the number of dang words that were removed from the message at that particular index.
 '''
 
+'''
+ListOfWords => ListOfWords
+" ".join([]) converts array into string
+'''
+def filter_messages(messages):
+    filtered_list = []
+    word_count = []
 
-def reverse_list(items):
-    return items[::-1]
+    for message in messages: #split list of messages into individual message 
+        temp_list = []
+        count = 0
+    
+        for word in message.split(): #split into individual words
+            if word == "dang":
+                count += 1
+            else:
+                temp_list.append(word)
+        word_count.append(count)
+        filtered_list.append(" ".join(temp_list))
+    
+    return filtered_list, word_count
