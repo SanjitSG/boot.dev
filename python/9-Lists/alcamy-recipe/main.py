82595f75577521cb5d5e2dabd7751bcfc3869cdb
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2025-07-03 21:42:33 +0530

    missing recipe

---
+++ python/9-Lists/alcamy-recipe/main.py
@@ -0,0 +1,13 @@
+
+def check_ingredient_match(recipe, ingredients):
+    counter = 0
+    missing_list = []
+    for item in recipe:
+       if item in ingredients:
+           counter += 1
+       else:
+           missing_list.append(item)   
+    percent = (counter/len(recipe))*100
+    
+    return percent, missing_list
+   
